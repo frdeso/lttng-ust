compudj/lttng-ust-dev

bytecode: initialize all contexts on trigger group creation
Rename `lttng_session_context_init()` to `lttng_context_init_all()` so
to reuse the code.

Signed-off-by: Francis Deslauriers <francis.deslauriers@efficios.com>
Change-Id: If80745677db993c1b38e158785886180d2e60d26

liblttng-ust/lttng-events.c
@@ -64,6 +64,7 @@
 #include "lttng-tracer.h"
 #include "lttng-tracer-core.h"
 #include "lttng-ust-statedump.h"
+#include "context-internal.h"
 #include "ust-events-internal.h"
 #include "wait.h"
 #include "../libringbuffer/shm.h"
@@ -153,7 +154,7 @@ struct lttng_session *lttng_session_create(void)
 	session = zmalloc(sizeof(struct lttng_session));
 	if (!session)
 		return NULL;
-	if (lttng_session_context_init(&session->ctx)) {
+	if (lttng_context_init_all(&session->ctx)) {
 		free(session);
 		return NULL;
 	}
@@ -177,6 +178,12 @@ struct lttng_trigger_group *lttng_trigger_group_create(void)
 	trigger_group = zmalloc(sizeof(struct lttng_trigger_group));
 	if (!trigger_group)
 		return NULL;
+
+	/* Add all contexts. */
+	if (lttng_context_init_all(&trigger_group->ctx)) {
+		free(trigger_group);
+		return NULL;
+	}
 
 	CDS_INIT_LIST_HEAD(&trigger_group->enablers_head);
 	CDS_INIT_LIST_HEAD(&trigger_group->triggers_head);

frdeso/lttng-ust

liblttng-ust/lttng-events.c
@@ -63,6 +63,7 @@
 #include "lttng-tracer.h"
 #include "lttng-tracer-core.h"
 #include "lttng-ust-statedump.h"
+#include "context-internal.h"
 #include "ust-events-internal.h"
 #include "wait.h"
 #include "../libringbuffer/shm.h"
@@ -154,7 +155,7 @@ struct lttng_session *lttng_session_create(void)
 	session = zmalloc(sizeof(struct lttng_session));
 	if (!session)
 		return NULL;
-	if (lttng_session_context_init(&session->ctx)) {
+	if (lttng_context_init_all(&session->ctx)) {
 		free(session);
 		return NULL;
 	}
@@ -178,6 +179,12 @@ struct lttng_event_notifier_group *lttng_event_notifier_group_create(void)
 	event_notifier_group = zmalloc(sizeof(struct lttng_event_notifier_group));
 	if (!event_notifier_group)
 		return NULL;
+
+	/* Add all contexts. */
+	if (lttng_context_init_all(&event_notifier_group->ctx)) {
+		free(event_notifier_group);
+		return NULL;
+	}
 
 	CDS_INIT_LIST_HEAD(&event_notifier_group->enablers_head);
 	CDS_INIT_LIST_HEAD(&event_notifier_group->event_notifiers_head);