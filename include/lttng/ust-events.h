compudj/lttng-ust-dev

liblttng-ust: add trigger probes and wire up
- Adds the trigger probe generation macros,
- Adds trigger enabling/disabling machinery,
- Adds trigger filter machinery,
- Adds trigger exclusion machinery,

The trigger system reuses lots of code from its event equivalent.

WIP

Signed-off-by: Francis Deslauriers <francis.deslauriers@efficios.com>
Change-Id: I570e46ddb4e341bd4967c9042ff17614b5bacb1b

include/lttng/ust-events.h
@@ -326,6 +326,7 @@ struct lttng_event_desc {
 	union {
 		struct {
 			const char **model_emf_uri;
+			void (*trigger_callback)(void);
 		} ext;
 		char padding[LTTNG_UST_EVENT_DESC_PADDING];
 	} u;
@@ -425,14 +426,19 @@ struct lttng_bytecode_runtime {
 	int link_failed;
 	struct cds_list_head node;	/* list of bytecode runtime in event */
 	/*
-	 * Pointer to a `struct lttng_session`-owned and URCU-protected
-	 * pointer.
+	 * Pointer to a URCU-protected pointer owned by an `struct
+	 * lttng_session`or `struct lttng_trigger_group`.
 	 */
 	struct lttng_ctx **pctx;
 };
 
 /*
- * Objects in a linked-list of enablers, owned by an event.
+ * Objects in a linked-list of enablers, owned by an event or trigger.
+ * This is used because an event (or a trigger) can be enabled by more than one
+ * enabler and we want a quick way to iterate over all enablers of an object.
+ *
+ * For example, event rules "my_app:a*" and "my_app:ab*" will both match the
+ * event with the name "my_app:abc".
  */
 struct lttng_enabler_ref {
 	struct cds_list_head node;		/* enabler ref list */
@@ -469,6 +475,19 @@ struct lttng_event {
 	struct cds_list_head enablers_ref_head;
 	struct cds_hlist_node hlist;	/* session ht of events */
 	int registered;			/* has reg'd tracepoint probe */
+};
+
+struct lttng_trigger {
+	uint64_t id;
+	int enabled;
+	int registered;			/* has reg'd tracepoint probe */
+	struct cds_list_head bytecode_runtime_head;
+	int has_enablers_without_bytecode;
+	struct cds_list_head enablers_ref_head;
+	const struct lttng_event_desc *desc;
+	struct cds_hlist_node hlist;	/* hashtable of triggers */
+	struct cds_list_head node;	/* Trigger list in session */
+	struct lttng_trigger_group *group; /* weak ref */
 };
 
 struct lttng_enum {
@@ -572,6 +591,12 @@ struct lttng_ust_event_ht {
 	struct cds_hlist_head table[LTTNG_UST_EVENT_HT_SIZE];
 };
 
+#define LTTNG_UST_TRIGGER_HT_BITS		12
+#define LTTNG_UST_TRIGGER_HT_SIZE		(1U << LTTNG_UST_TRIGGER_HT_BITS)
+struct lttng_ust_trigger_ht {
+	struct cds_hlist_head table[LTTNG_UST_TRIGGER_HT_SIZE];
+};
+
 #define LTTNG_UST_ENUM_HT_BITS		12
 #define LTTNG_UST_ENUM_HT_SIZE		(1U << LTTNG_UST_ENUM_HT_BITS)
 
@@ -618,6 +643,9 @@ struct lttng_trigger_group {
 	int notification_fd;
 	struct cds_list_head node;		/* Trigger group handle list */
 	struct cds_list_head enablers_head;
+	struct cds_list_head triggers_head;	/* list of triggers */
+	struct lttng_ust_trigger_ht triggers_ht; /* hashtable of triggers */
+	struct lttng_ctx *ctx;			/* contexts for filters. */
 };
 
 struct lttng_transport {
@@ -632,6 +660,8 @@ int lttng_session_enable(struct lttng_session *session);
 int lttng_session_disable(struct lttng_session *session);
 int lttng_session_statedump(struct lttng_session *session);
 void lttng_session_destroy(struct lttng_session *session);
+
+void lttng_trigger_send_notification(struct lttng_trigger *trigger);
 
 struct lttng_channel *lttng_channel_create(struct lttng_session *session,
 				       const char *transport_name,

liblttng-ust/context-provider-internal.h
@@ -1,0 +1,37 @@
+#ifndef _LTTNG_UST_CONTEXT_PROVIDER_INTERNAL_H
+#define _LTTNG_UST_CONTEXT_PROVIDER_INTERNAL_H
+
+/*
+ * Copyright 2019 - Francis Deslauriers <francis.deslauriers@efficios.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <stddef.h>
+#include <lttng/ust-events.h>
+
+void lttng_ust_context_set_trigger_group_provider(const char *name,
+		size_t (*get_size)(struct lttng_ctx_field *field, size_t offset),
+		void (*record)(struct lttng_ctx_field *field,
+			struct lttng_ust_lib_ring_buffer_ctx *ctx,
+			struct lttng_channel *chan),
+		void (*get_value)(struct lttng_ctx_field *field,
+			struct lttng_ctx_value *value));
+
+#endif /* _LTTNG_UST_CONTEXT_PROVIDER_INTERNAL_H */

liblttng-ust/lttng-context-provider.c
@@ -27,8 +27,10 @@
 #include <unistd.h>
 
 #include <lttng/ust-context-provider.h>
+
 #include "lttng-tracer-core.h"
 #include "jhash.h"
+#include "context-provider-internal.h"
 #include <helper.h>
 
 #define CONTEXT_PROVIDER_HT_BITS	12
@@ -90,7 +92,12 @@ int lttng_ust_context_provider_register(struct lttng_ust_context_provider *provi
 	hash = jhash(provider->name, name_len, 0);
 	head = &context_provider_ht.table[hash & (CONTEXT_PROVIDER_HT_SIZE - 1)];
 	cds_hlist_add_head(&provider->node, head);
+
 	lttng_ust_context_set_session_provider(provider->name,
+		provider->get_size, provider->record,
+		provider->get_value);
+
+	lttng_ust_context_set_trigger_group_provider(provider->name,
 		provider->get_size, provider->record,
 		provider->get_value);
 end:
@@ -107,6 +114,11 @@ void lttng_ust_context_provider_unregister(struct lttng_ust_context_provider *pr
 	lttng_ust_context_set_session_provider(provider->name,
 		lttng_ust_dummy_get_size, lttng_ust_dummy_record,
 		lttng_ust_dummy_get_value);
+
+	lttng_ust_context_set_trigger_group_provider(provider->name,
+		lttng_ust_dummy_get_size, lttng_ust_dummy_record,
+		lttng_ust_dummy_get_value);
+
 	cds_hlist_del(&provider->node);
 end:
 	ust_unlock();

liblttng-ust/lttng-ust-abi.c
@@ -38,6 +38,7 @@
  */
 
 #define _LGPL_SOURCE
+#include <fcntl.h>
 #include <stdint.h>
 #include <unistd.h>
 
@@ -346,11 +347,24 @@ static
 int lttng_abi_trigger_send_fd(void *owner, int trigger_notif_fd)
 {
 	struct lttng_trigger_group *trigger_group;
-	int trigger_group_objd, ret;
+	int trigger_group_objd, ret, fd_flag, close_ret;
 
 	trigger_group = lttng_trigger_group_create();
 	if (!trigger_group)
 		return -ENOMEM;
+
+	/*
+	 * Set this file descriptor as NON-BLOCKING.
+	 */
+	fd_flag = fcntl(trigger_notif_fd, F_GETFL);
+
+	fd_flag |= O_NONBLOCK;
+
+	ret = fcntl(trigger_notif_fd, F_SETFL, fd_flag);
+	if (ret) {
+		ret = -errno;
+		goto fd_error;
+	}
 
 	trigger_group_objd = objd_alloc(trigger_group,
 		&lttng_trigger_group_ops, owner, "trigger_group");
@@ -367,6 +381,12 @@ int lttng_abi_trigger_send_fd(void *owner, int trigger_notif_fd)
 
 objd_error:
 	lttng_trigger_group_destroy(trigger_group);
+fd_error:
+	close_ret = close(trigger_notif_fd);
+	if (close_ret) {
+		PERROR("close");
+	}
+
 	return ret;
 }
 
@@ -652,7 +672,7 @@ static int lttng_ust_trigger_enabler_create(int trigger_group_obj, void *owner,
 		struct lttng_ust_trigger *trigger_param,
 		enum lttng_enabler_format_type type)
 {
-	struct lttng_trigger_group *group_handle =
+	struct lttng_trigger_group *trigger_group =
 		objd_private(trigger_group_obj);
 	struct lttng_trigger_enabler *trigger_enabler;
 	int trigger_objd, ret;
@@ -665,7 +685,8 @@ static int lttng_ust_trigger_enabler_create(int trigger_group_obj, void *owner,
 		goto objd_error;
 	}
 
-	trigger_enabler = lttng_trigger_enabler_create(group_handle, type, trigger_param);
+	trigger_enabler = lttng_trigger_enabler_create(trigger_group, type,
+		trigger_param);
 	if (!trigger_enabler) {
 		ret = -ENOMEM;
 		goto trigger_error;

frdeso/lttng-ust

include/lttng/ust-events.h
@@ -360,6 +360,7 @@ struct lttng_event_desc {
 	union {
 		struct {
 			const char **model_emf_uri;
+			void (*event_notifier_callback)(void);
 		} ext;
 		char padding[LTTNG_UST_EVENT_DESC_PADDING];
 	} u;
@@ -446,14 +447,20 @@ struct lttng_bytecode_runtime {
 	int link_failed;
 	struct cds_list_head node;	/* list of bytecode runtime in event */
 	/*
-	 * Pointer to a `struct lttng_session`-owned and URCU-protected
-	 * pointer.
+	 * Pointer to a URCU-protected pointer owned by an `struct
+	 * lttng_session`or `struct lttng_event_notifier_group`.
 	 */
 	struct lttng_ctx **pctx;
 };
 
 /*
- * Objects in a linked-list of enablers, owned by an event.
+ * Objects in a linked-list of enablers, owned by an event or event_notifier.
+ * This is used because an event (or a event_notifier) can be enabled by more
+ * than one enabler and we want a quick way to iterate over all enablers of an
+ * object.
+ *
+ * For example, event rules "my_app:a*" and "my_app:ab*" will both match the
+ * event with the name "my_app:abc".
  */
 struct lttng_enabler_ref {
 	struct cds_list_head node;		/* enabler ref list */
@@ -490,6 +497,19 @@ struct lttng_event {
 	struct cds_list_head enablers_ref_head;
 	struct cds_hlist_node hlist;	/* session ht of events */
 	int registered;			/* has reg'd tracepoint probe */
+};
+
+struct lttng_event_notifier {
+	uint64_t user_token;
+	int enabled;
+	int registered;			/* has reg'd tracepoint probe */
+	struct cds_list_head filter_bytecode_runtime_head;
+	int has_enablers_without_bytecode;
+	struct cds_list_head enablers_ref_head;
+	const struct lttng_event_desc *desc;
+	struct cds_hlist_node hlist;	/* hashtable of event_notifiers */
+	struct cds_list_head node;	/* event_notifier list in session */
+	struct lttng_event_notifier_group *group; /* weak ref */
 };
 
 struct lttng_enum {
@@ -600,6 +620,12 @@ struct lttng_ust_event_ht {
 	struct cds_hlist_head table[LTTNG_UST_EVENT_HT_SIZE];
 };
 
+#define LTTNG_UST_EVENT_NOTIFIER_HT_BITS		12
+#define LTTNG_UST_EVENT_NOTIFIER_HT_SIZE		(1U << LTTNG_UST_EVENT_NOTIFIER_HT_BITS)
+struct lttng_ust_event_notifier_ht {
+	struct cds_hlist_head table[LTTNG_UST_EVENT_NOTIFIER_HT_SIZE];
+};
+
 #define LTTNG_UST_ENUM_HT_BITS		12
 #define LTTNG_UST_ENUM_HT_SIZE		(1U << LTTNG_UST_ENUM_HT_BITS)
 
@@ -646,6 +672,9 @@ struct lttng_event_notifier_group {
 	int notification_fd;
 	struct cds_list_head node;		/* Event notifier group handle list */
 	struct cds_list_head enablers_head;
+	struct cds_list_head event_notifiers_head;	/* list of event_notifiers */
+	struct lttng_ust_event_notifier_ht event_notifiers_ht; /* hashtable of event_notifiers */
+	struct lttng_ctx *ctx;			/* contexts for filters. */
 };
 
 struct lttng_transport {
@@ -660,6 +689,9 @@ int lttng_session_enable(struct lttng_session *session);
 int lttng_session_disable(struct lttng_session *session);
 int lttng_session_statedump(struct lttng_session *session);
 void lttng_session_destroy(struct lttng_session *session);
+
+void lttng_event_notifier_notification_send(
+		struct lttng_event_notifier *event_notifier);
 
 struct lttng_channel *lttng_channel_create(struct lttng_session *session,
 				       const char *transport_name,

liblttng-ust/context-provider-internal.h
@@ -1,0 +1,37 @@
+#ifndef _LTTNG_UST_CONTEXT_PROVIDER_INTERNAL_H
+#define _LTTNG_UST_CONTEXT_PROVIDER_INTERNAL_H
+
+/*
+ * Copyright 2019 - Francis Deslauriers <francis.deslauriers@efficios.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <stddef.h>
+#include <lttng/ust-events.h>
+
+void lttng_ust_context_set_event_notifier_group_provider(const char *name,
+		size_t (*get_size)(struct lttng_ctx_field *field, size_t offset),
+		void (*record)(struct lttng_ctx_field *field,
+			struct lttng_ust_lib_ring_buffer_ctx *ctx,
+			struct lttng_channel *chan),
+		void (*get_value)(struct lttng_ctx_field *field,
+			struct lttng_ctx_value *value));
+
+#endif /* _LTTNG_UST_CONTEXT_PROVIDER_INTERNAL_H */

liblttng-ust/lttng-context-provider.c
@@ -27,8 +27,10 @@
 #include <unistd.h>
 
 #include <lttng/ust-context-provider.h>
+
 #include "lttng-tracer-core.h"
 #include "jhash.h"
+#include "context-provider-internal.h"
 #include <helper.h>
 
 #define CONTEXT_PROVIDER_HT_BITS	12
@@ -90,7 +92,12 @@ int lttng_ust_context_provider_register(struct lttng_ust_context_provider *provi
 	hash = jhash(provider->name, name_len, 0);
 	head = &context_provider_ht.table[hash & (CONTEXT_PROVIDER_HT_SIZE - 1)];
 	cds_hlist_add_head(&provider->node, head);
+
 	lttng_ust_context_set_session_provider(provider->name,
+		provider->get_size, provider->record,
+		provider->get_value);
+
+	lttng_ust_context_set_event_notifier_group_provider(provider->name,
 		provider->get_size, provider->record,
 		provider->get_value);
 end:
@@ -107,6 +114,11 @@ void lttng_ust_context_provider_unregister(struct lttng_ust_context_provider *pr
 	lttng_ust_context_set_session_provider(provider->name,
 		lttng_ust_dummy_get_size, lttng_ust_dummy_record,
 		lttng_ust_dummy_get_value);
+
+	lttng_ust_context_set_event_notifier_group_provider(provider->name,
+		lttng_ust_dummy_get_size, lttng_ust_dummy_record,
+		lttng_ust_dummy_get_value);
+
 	cds_hlist_del(&provider->node);
 end:
 	ust_unlock();

liblttng-ust/lttng-ust-abi.c
@@ -38,6 +38,7 @@
  */
 
 #define _LGPL_SOURCE
+#include <fcntl.h>
 #include <stdint.h>
 #include <unistd.h>
 
@@ -346,11 +347,24 @@ static
 int lttng_abi_event_notifier_send_fd(void *owner, int event_notifier_notif_fd)
 {
 	struct lttng_event_notifier_group *event_notifier_group;
-	int event_notifier_group_objd, ret;
+	int event_notifier_group_objd, ret, fd_flag, close_ret;
 
 	event_notifier_group = lttng_event_notifier_group_create();
 	if (!event_notifier_group)
 		return -ENOMEM;
+
+	/*
+	 * Set this file descriptor as NON-BLOCKING.
+	 */
+	fd_flag = fcntl(event_notifier_notif_fd, F_GETFL);
+
+	fd_flag |= O_NONBLOCK;
+
+	ret = fcntl(event_notifier_notif_fd, F_SETFL, fd_flag);
+	if (ret) {
+		ret = -errno;
+		goto fd_error;
+	}
 
 	event_notifier_group_objd = objd_alloc(event_notifier_group,
 		&lttng_event_notifier_group_ops, owner, "event_notifier_group");
@@ -367,6 +381,12 @@ int lttng_abi_event_notifier_send_fd(void *owner, int event_notifier_notif_fd)
 
 objd_error:
 	lttng_event_notifier_group_destroy(event_notifier_group);
+fd_error:
+	close_ret = close(event_notifier_notif_fd);
+	if (close_ret) {
+		PERROR("close");
+	}
+
 	return ret;
 }
 
@@ -652,7 +672,7 @@ static int lttng_ust_event_notifier_enabler_create(int event_notifier_group_obj,
 		void *owner, struct lttng_ust_event_notifier *event_notifier_param,
 		enum lttng_enabler_format_type type)
 {
-	struct lttng_event_notifier_group *group_handle =
+	struct lttng_event_notifier_group *event_notifier_group =
 		objd_private(event_notifier_group_obj);
 	struct lttng_event_notifier_enabler *event_notifier_enabler;
 	int event_notifier_objd, ret;
@@ -665,7 +685,8 @@ static int lttng_ust_event_notifier_enabler_create(int event_notifier_group_obj,
 		goto objd_error;
 	}
 
-	event_notifier_enabler = lttng_event_notifier_enabler_create(group_handle, type, event_notifier_param);
+	event_notifier_enabler = lttng_event_notifier_enabler_create(
+		event_notifier_group, type, event_notifier_param);
 	if (!event_notifier_enabler) {
 		ret = -ENOMEM;
 		goto event_notifier_error;