compudj/lttng-ust-dev

Implement capturing payload on triggers
This commit allows the user to request a subset of payload fields of the
tracepoint to be captured when a trigger is fired and sent back the session
daemon as part of the notification.

Captures are really similar to filter in the way they work. Like with
filters, the sessiond attaches to a trigger one bytecode program per
field it wants to capture using the following function:

  int ustctl_set_capture(int sock, struct lttng_ust_capture_bytecode *bytecode,
       	 struct lttng_ust_object_data *obj_data);

The capture bytecode program is then validated and specialized for its
target tracepoint just like filter bytecode programs.

The major difference between filters and captures is with what they
return. Filters can only return _RECORD or _DISCARD for match and
no-match respectively. Captures also need to return captured values. For
this reason, we added an output parameter to the capture interpreter
function that value can be extracted.

Here is the signature of the capture interpreter function:
  uint64_t lttng_bytecode_capture_interpret(void *capture_data,
       	 const char *capture_stack_data,
       	 void *output)

When comes the time to send the notification, every capture bytecode
program is executed in turns and its output value is recorded to the
capture buffer to be sent back to the Sessiond.

Signed-off-by: Francis Deslauriers <francis.deslauriers@efficios.com>
Change-Id: Ia3023a4f5c0aec014b37b72f92d1a924c7eff6a2

liblttng-ust/lttng-events.c
@@ -814,6 +814,7 @@ int lttng_trigger_create(const struct lttng_event_desc *desc,
 	trigger->registered = 0;
 
 	CDS_INIT_LIST_HEAD(&trigger->filter_bytecode_runtime_head);
+	CDS_INIT_LIST_HEAD(&trigger->capture_bytecode_runtime_head);
 	CDS_INIT_LIST_HEAD(&trigger->enablers_ref_head);
 	trigger->desc = desc;
 
@@ -1355,9 +1356,11 @@ struct lttng_trigger_enabler *lttng_trigger_enabler_create(
 		return NULL;
 	trigger_enabler->base.format_type = format_type;
 	CDS_INIT_LIST_HEAD(&trigger_enabler->base.filter_bytecode_head);
+	CDS_INIT_LIST_HEAD(&trigger_enabler->capture_bytecode_head);
 	CDS_INIT_LIST_HEAD(&trigger_enabler->base.excluder_head);
 
 	trigger_enabler->id = trigger_param->id;
+	trigger_enabler->num_captures = 0;
 
 	memcpy(&trigger_enabler->base.event_param.name, trigger_param->name,
 		sizeof(trigger_enabler->base.event_param.name));
@@ -1449,6 +1452,18 @@ int lttng_trigger_enabler_attach_filter_bytecode(
 {
 	_lttng_enabler_attach_filter_bytecode(
 		lttng_trigger_enabler_as_enabler(trigger_enabler), bytecode);
+
+	lttng_trigger_group_sync_enablers(trigger_enabler->group);
+	return 0;
+}
+
+int lttng_trigger_enabler_attach_capture_bytecode(
+		struct lttng_trigger_enabler *trigger_enabler,
+		struct lttng_ust_bytecode_node *bytecode)
+{
+	bytecode->enabler = lttng_trigger_enabler_as_enabler(trigger_enabler);
+	cds_list_add_tail(&bytecode->node, &trigger_enabler->capture_bytecode_head);
+	trigger_enabler->num_captures++;
 
 	lttng_trigger_group_sync_enablers(trigger_enabler->group);
 	return 0;
@@ -1645,6 +1660,7 @@ void lttng_create_trigger_if_missing(struct lttng_trigger_enabler *trigger_enabl
 			struct cds_hlist_node *node;
 
 			desc = probe_desc->event_desc[i];
+
 			if (!lttng_desc_match_enabler(desc,
 					lttng_trigger_enabler_as_enabler(trigger_enabler)))
 				continue;
@@ -1737,6 +1753,14 @@ int lttng_trigger_enabler_ref_triggers(struct lttng_trigger_enabler *trigger_ena
 		lttng_enabler_link_bytecode(trigger->desc,
 			&trigger_group->ctx, &trigger->filter_bytecode_runtime_head,
 			&lttng_trigger_enabler_as_enabler(trigger_enabler)->filter_bytecode_head);
+
+		/*
+		 * Link capture bytecodes if not linked yet.
+		 */
+		lttng_enabler_link_bytecode(trigger->desc,
+			&trigger_group->ctx, &trigger->capture_bytecode_runtime_head,
+			&trigger_enabler->capture_bytecode_head);
+		trigger->num_captures = trigger_enabler->num_captures;
 	}
 end:
 	return 0;
@@ -1798,6 +1822,12 @@ void lttng_trigger_group_sync_enablers(struct lttng_trigger_group *trigger_group
 		cds_list_for_each_entry(runtime,
 				&trigger->filter_bytecode_runtime_head, node) {
 			lttng_bytecode_filter_sync_state(runtime);
+		}
+
+		/* Enable captures. */
+		cds_list_for_each_entry(runtime,
+				&trigger->capture_bytecode_runtime_head, node) {
+			lttng_bytecode_capture_sync_state(runtime);
 		}
 	}
 	__tracepoint_probe_prune_release_queue();

frdeso/lttng-ust

liblttng-ust/lttng-events.c
@@ -826,6 +826,7 @@ int lttng_event_notifier_create(const struct lttng_event_desc *desc,
 	event_notifier->registered = 0;
 
 	CDS_INIT_LIST_HEAD(&event_notifier->filter_bytecode_runtime_head);
+	CDS_INIT_LIST_HEAD(&event_notifier->capture_bytecode_runtime_head);
 	CDS_INIT_LIST_HEAD(&event_notifier->enablers_ref_head);
 	event_notifier->desc = desc;
 
@@ -1368,9 +1369,11 @@ struct lttng_event_notifier_enabler *lttng_event_notifier_enabler_create(
 		return NULL;
 	event_notifier_enabler->base.format_type = format_type;
 	CDS_INIT_LIST_HEAD(&event_notifier_enabler->base.filter_bytecode_head);
+	CDS_INIT_LIST_HEAD(&event_notifier_enabler->capture_bytecode_head);
 	CDS_INIT_LIST_HEAD(&event_notifier_enabler->base.excluder_head);
 
 	event_notifier_enabler->user_token = event_notifier_param->event.token;
+	event_notifier_enabler->num_captures = 0;
 
 	memcpy(&event_notifier_enabler->base.event_param.name,
 		event_notifier_param->event.name,
@@ -1470,6 +1473,20 @@ int lttng_event_notifier_enabler_attach_filter_bytecode(
 	_lttng_enabler_attach_filter_bytecode(
 		lttng_event_notifier_enabler_as_enabler(event_notifier_enabler),
 		bytecode);
+
+	lttng_event_notifier_group_sync_enablers(event_notifier_enabler->group);
+	return 0;
+}
+
+int lttng_event_notifier_enabler_attach_capture_bytecode(
+		struct lttng_event_notifier_enabler *event_notifier_enabler,
+		struct lttng_ust_bytecode_node *bytecode)
+{
+	bytecode->enabler = lttng_event_notifier_enabler_as_enabler(
+			event_notifier_enabler);
+	cds_list_add_tail(&bytecode->node,
+			&event_notifier_enabler->capture_bytecode_head);
+	event_notifier_enabler->num_captures++;
 
 	lttng_event_notifier_group_sync_enablers(event_notifier_enabler->group);
 	return 0;
@@ -1670,6 +1687,7 @@ void lttng_create_event_notifier_if_missing(
 			struct cds_hlist_node *node;
 
 			desc = probe_desc->event_desc[i];
+
 			if (!lttng_desc_match_enabler(desc,
 					lttng_event_notifier_enabler_as_enabler(event_notifier_enabler)))
 				continue;
@@ -1765,6 +1783,15 @@ int lttng_event_notifier_enabler_ref_event_notifiers(
 			&event_notifier_group->ctx,
 			&event_notifier->filter_bytecode_runtime_head,
 			&lttng_event_notifier_enabler_as_enabler(event_notifier_enabler)->filter_bytecode_head);
+
+		/*
+		 * Link capture bytecodes if not linked yet.
+		 */
+		lttng_enabler_link_bytecode(event_notifier->desc,
+			&event_notifier_group->ctx, &event_notifier->capture_bytecode_runtime_head,
+			&event_notifier_enabler->capture_bytecode_head);
+
+		event_notifier->num_captures = event_notifier_enabler->num_captures;
 	}
 end:
 	return 0;
@@ -1826,6 +1853,12 @@ void lttng_event_notifier_group_sync_enablers(struct lttng_event_notifier_group 
 		cds_list_for_each_entry(runtime,
 				&event_notifier->filter_bytecode_runtime_head, node) {
 			lttng_bytecode_filter_sync_state(runtime);
+		}
+
+		/* Enable captures. */
+		cds_list_for_each_entry(runtime,
+				&event_notifier->capture_bytecode_runtime_head, node) {
+			lttng_bytecode_capture_sync_state(runtime);
 		}
 	}
 	__tracepoint_probe_prune_release_queue();